98f958b796ad6471502cb138ebdfd2649b346405
Paul Höhn <paul.hoehn@dlr.de>
2023-08-26 07:53:06 +0200

    Updating file names and structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="satellite_datacube",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'rasterio',
+        'matplotlib',
+        'json', 
+        'h5py',
+        're',
+        'fiona',
+        'pandas',
+    ],
+    author="Paul Höhn",
+    author_email="paul.hoen@outlook.de",
+    description="A package to handle satellite image time series with data cubes.",
+    license="MIT",
+    keywords="satellite datacube",
+    url="https://github.com/PaulH97/SatelliteDataCube.git",
+)
